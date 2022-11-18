bd02cb664ad577b0f90b0e0ddbdec98ce56d81ea
Flux <Fluqz@users.noreply.github.com>
2022-11-18 16:18:27 +0100

    init repository

---
+++ src/game-of-life.ts
@@ -0,0 +1,181 @@
+import * as THREE from 'three'
+
+export interface Cell {
+    state?: boolean
+    neighbours?: number
+}
+
+export class GameOfLife {
+
+    width: number
+    height: number
+    depth: number
+
+    // Size of cells
+    cellSize
+
+    // How likely for a cell to be alive at start (in percentage)
+    probabilityOfAliveAtStart
+
+    // Array of cells
+    cells: Cell[][][] = []
+    // Buffer to record the state of the cells and use this 
+    // while changing the others in the interations
+    cellsBuffer: Cell[][][] = []
+
+    generation: number = 0
+
+    done: boolean = false
+
+    _changed: boolean = false
+
+
+    constructor(w?:number, h?:number, d?:number, divition?:number, probabilityOfAliveAtStart?: number) {
+
+        this.width = w ? w : 50
+        this.height = h ? h : 50
+        this.depth = d ? d : 50
+        this.cellSize = divition ? divition : 1
+        this.probabilityOfAliveAtStart = probabilityOfAliveAtStart ? probabilityOfAliveAtStart : 5
+
+        this.init()
+    }
+
+    init = () => {
+
+        // Save cells to buffer (so we opeate with one array keeping the other intact)
+        for (let x = 0; x < this.width / this.cellSize; x++) {
+
+            this.cells[x] = []
+            this.cellsBuffer[x] = []
+
+            for (let y = 0; y < this.height / this.cellSize; y++) {
+                
+                this.cells[x][y] = []
+                this.cellsBuffer[x][y] = []
+
+                for (let z = 0; z < this.depth / this.cellSize; z++) {
+
+                    this.cells[x][y][z] = {}
+                    this.cellsBuffer[x][y][z] = {}
+                }
+            }
+        }
+
+        this.reset()
+    }
+
+    iterate = () => { // When the clock ticks
+
+        this.generation++
+
+        // Save cells to buffer (so we opeate with one array keeping the other intact)
+        for (let x = 0; x < this.width / this.cellSize; x++) {
+
+            for (let y = 0; y < this.height / this.cellSize; y++) {
+                
+                for (let z = 0; z < this.depth / this.cellSize; z++) {
+
+                    this.cellsBuffer[x][y][z].state = this.cells[x][y][z].state
+                    this.cellsBuffer[x][y][z].neighbours = this.cells[x][y][z].neighbours
+                }
+            }
+        }
+
+        this._changed = false
+
+        // Visit each cell:
+        for (let x = 0; x < this.width / this.cellSize; x++) {
+
+            for (let y = 0; y < this.height / this.cellSize; y++) {
+            
+                for (let z = 0; z < this.depth / this.cellSize; z++) {
+            
+                    // And visit all the neighbours of each cell
+                    let neighbours = 0; // We'll count the neighbours
+                    let n = 0
+                    for (let xx = x - 1; xx <= x + 1; xx++) {
+
+                        for (let yy = y - 1; yy <= y + 1; yy++) {  
+
+                            for (let zz = z - 1; zz <= z + 1; zz++) {  
+
+                                if (((xx >= 0) && (xx < this.width / this.cellSize)) && 
+                                    ((yy >= 0) && (yy < this.height / this.cellSize)) &&
+                                    ((zz >= 0) && (zz < this.depth / this.cellSize))) { // Make sure you are not out of bounds
+
+                                    if (!(x == x && yy == y && zz == z)) { // Make sure to to check against self
+
+                                        if (this.cellsBuffer[xx][yy][zz].state == true) {
+
+                                            neighbours++; // Check alive neighbours and count them
+
+                                            this._changed = true
+                                        }
+                                    }
+                                }
+                            } 
+                        } 
+                    }
+
+                    this.cells[x][y][z].neighbours = neighbours
+
+                    this.checkRules(this.cells[x][y][z], this.cellsBuffer[x][y][z])
+                }
+            }
+        }
+
+        if(this._changed == false) this.done = true
+    } 
+
+    checkRules(cell: Cell, oldCell: Cell) {
+
+        // We've checked the neigbours: apply rules!
+        if (oldCell.state == true) { // The cell is alive: kill it if necessary
+
+            // ONE OR THE OTHER OR BOTH
+            // if (cell.neighbours <= 6) cell.state = false // Die unless it has 2 or 3 neighbours
+            if (cell.neighbours < 4 || cell.neighbours > 6) cell.state = false // Die unless it has 2 or 3 neighbours
+        }
+        else { // The cell is dead: make it live if necessary  
+
+            // ONE OR THE OTHER OR BOTH
+            // if (cell.neighbours > 6) cell.state = true // Only if it has 3 neighbours
+            if (cell.neighbours == 5 ) cell.state = true // Only if it has 3 neighbours
+        }
+    }
+
+    reset = () => {
+        
+        // Initialization of cells
+        for (let x = 0; x < this.width / this.cellSize; x++) {
+            
+            for (let y = 0; y < this.height / this.cellSize; y++) {
+            
+                for (let z = 0; z < this.depth / this.cellSize; z++) {
+
+                    let state: boolean
+                    if (Math.random() * 100 < this.probabilityOfAliveAtStart) state = true
+                    else state = false
+                    this.cells[x][y][z].state = state // Save state of each cell
+                }
+            }
+        }
+    }
+
+    clear = () => {
+
+        console.log('clear')
+        // Initialization of cells
+        for (let x = 0; x < this.width / this.cellSize; x++) {
+            
+            for (let y = 0; y < this.height / this.cellSize; y++) {
+            
+                for (let z = 0; z < this.depth / this.cellSize; z++) {
+
+                    this.cells[x][y][z].state = false
+                }
+            }
+        }
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,206 @@
+import { AmbientLight, BufferGeometry, Color, InstancedMesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer, BoxGeometry, Matrix4, GridHelper, AxesHelper, Group, Vector2, MeshNormalMaterial, DirectionalLight, MeshStandardMaterial, Material, Mesh } from "three"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { GameOfLife } from "./game-of-life"
+
+import './style.css'
+
+let container: HTMLElement
+let renderer: WebGLRenderer
+let camera: PerspectiveCamera
+let scene: Scene
+let orbit: OrbitControls
+
+let group: Group
+
+let width: number
+let height: number
+let ratio: number
+
+let light
+
+  // Variables for timer
+let interval = 50
+let lastRecordedTime = Date.now()
+
+  // Colors for active/inactive cells
+let alive: Color = new Color(0, 200, 0)
+let dead: Color = new Color(0, 0, 0)
+
+let gol: GameOfLife
+
+    // Pause
+let pause: boolean = false
+
+let instacedMesh: InstancedMesh
+let geometry: BufferGeometry
+let material: Material
+
+let boundingbox: Mesh
+
+let cubeSide: number = 80
+
+const init = () => {
+
+  container = document.querySelector('#webGL')
+
+  width = window.innerWidth
+  height = window.innerHeight
+  ratio = window.devicePixelRatio
+  
+  renderer = new WebGLRenderer({ antialias: true })
+  renderer.setClearColor(0xEEEEEE)
+  camera = new PerspectiveCamera(75, width / height, .1, 1000)
+  scene = new Scene()
+  orbit = new OrbitControls(camera, renderer.domElement)
+
+  container.append(renderer.domElement)
+
+  group = new Group()
+  scene.add(group)
+
+  light = new DirectionalLight(0xFFFFFF, .7)
+  light.position.set(-100, 300, -100)
+  scene.add(light)
+
+  gol = new GameOfLife(cubeSide, cubeSide, cubeSide)
+
+  boundingbox = new Mesh(new BoxGeometry(cubeSide, cubeSide, cubeSide), new MeshBasicMaterial({ color: 0x000000, wireframe: true }))
+
+  scene.add(boundingbox)
+
+  // scene.add(new GridHelper(gol.width, gol.width / gol.cellSize))
+  camera.position.set(gol.width, gol.height, gol.depth)
+
+  geometry = new BoxGeometry(gol.cellSize, gol.cellSize, gol.cellSize)
+  geometry.translate(gol.cellSize / 2, gol.cellSize / 2, gol.cellSize / 2)
+  material = new MeshNormalMaterial()
+
+  material.onBeforeCompile = ( shader ) => {
+
+    shader.uniforms.textures = {
+      time: { value: 1.0 },
+		  resolution: { value: new Vector2(width, height) }
+    }
+
+    shader.vertexShader = shader.vertexShader
+
+    shader.fragmentShader = shader.fragmentShader
+  }
+  
+  instacedMesh = new InstancedMesh(geometry, material, gol.width * gol.height * gol.depth)
+  group.add(instacedMesh)
+
+  group.position.set(-gol.width * gol.cellSize / 2, -gol.height * gol.cellSize / 2, -gol.depth * gol.cellSize / 2)
+
+  let index = 0
+  let matrix = new Matrix4()
+  for (let x = 0; x < gol.width / gol.cellSize; x++) {
+
+    for (let y = 0; y < gol.height / gol.cellSize; y++) {
+
+      for (let z = 0; z < gol.depth / gol.cellSize; z++) {
+
+        matrix.setPosition(x * gol.cellSize, y * gol.cellSize, z * gol.cellSize)
+
+        instacedMesh.setMatrixAt(index, matrix)
+
+        index++
+      }
+    }
+  }
+
+  resize()
+
+  draw()
+
+  window.addEventListener('resize', resize)
+}
+
+const loop = () => {
+
+  requestAnimationFrame(loop)
+  if(Date.now() - lastRecordedTime >= interval) {
+
+    console.log('gen', gol.generation)
+    step()
+    lastRecordedTime = Date.now()
+  }
+
+  orbit.update()
+
+  renderer.render(scene, camera)
+}
+
+const resize = () => {
+
+  width = window.innerWidth
+  height = window.innerHeight
+  ratio = window.devicePixelRatio
+
+  camera.aspect = width / height
+  camera.updateProjectionMatrix()
+
+  renderer.setSize(width, height)
+  renderer.setPixelRatio(ratio)
+}
+
+const step = () => {
+
+  if(pause) return
+
+  gol.iterate()
+
+  draw()
+
+  if(gol.done) gol.reset()
+}
+
+const draw = () => {
+
+  let index = 0
+  let matrix = new Matrix4()
+
+  //Draw grid
+  for (let x = 0; x < gol.width / gol.cellSize; x++) {
+
+    for (let y = 0; y < gol.height / gol.cellSize; y++) {
+
+      for (let z = 0; z < gol.depth / gol.cellSize; z++) {
+
+        if (gol.cellsBuffer[x][y][z] != gol.cells[x][y][z] && gol.cells[x][y][z].state == true) {
+
+          matrix.setPosition(x * gol.cellSize, y * gol.cellSize, z * gol.cellSize)
+          instacedMesh.setMatrixAt(index, matrix)
+
+          instacedMesh.instanceMatrix.needsUpdate = true
+        }
+        else if(gol.cellsBuffer[x][y][z] != gol.cells[x][y][z]) {
+          // If dead
+          matrix.setPosition(-100 * cubeSide, 100 * cubeSide, 100 * cubeSide)
+          instacedMesh.setMatrixAt(index, matrix)
+
+          instacedMesh.instanceMatrix.needsUpdate = true
+        }
+
+        index++
+      }
+    }
+  }
+}
+
+let togglePause = () => {
+
+  pause = !pause
+}
+
+init()
+loop()
+
+document.addEventListener('mousedown', () => {
+
+
+  // gol.clear()
+})
+
+
+export {}
